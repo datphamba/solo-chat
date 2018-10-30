575eeba666cf3a27d9363ae6d62663acc398783b
datphamba013 <datphamba.013@gmail.com>
2018-10-30 19:24:05 +0700

    last commit

--- src/Messager.js
+++ src/Messager.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
-const Message = ({chat, user}) => (
-	<li className="chat left" 
-		style={{ background: `${user === chat.username ? "#2729FF" : "#f3f3f3"}`, color: `${user === chat.username ? "white" : "black"}` }}>
-		<img src="https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-9/40504683_2170866303170588_483742712009326592_n.jpg?_nc_cat=107&_nc_ht=scontent.fsgn5-2.fna&oh=619339b7e2e2d2a238939abdecc50a5c&oe=5C88750F" alt={`${chat.username}'s profile pic`} />
+const Message = ({chat}) => (
+	<li className="chat left" >
+		<img src="https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-9/40504683_2170866303170588_483742712009326592_n.jpg?_nc_cat=107&_nc_ht=scontent.fsgn5-2.fna&oh=619339b7e2e2d2a238939abdecc50a5c&oe=5C88750F" />
         {chat}
     </li>
 );
